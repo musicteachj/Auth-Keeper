56a7c872f18b9eafb85bb3d6de17eb0f51273e32
musicteachj <jimilittle3@hotmail.com>
2017-10-17 18:59:23 -0400

    test
    test

--- client/src/components/default.js
+++ client/src/components/default.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <div className="default-content">default landing content goes here...</div>;
+export default () => <div className="default-content">HEY HEY HEY</div>;
